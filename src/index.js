12d2c3855619f1c4a8c461d30d650b8353f080c2
Manish Kumar Pandey <896849@cognizant.com>
2024-02-09 13:56:08 +0530

    updated the changes for index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
     {
@@ -50,40 +51,82 @@ const pizzaData = [
 function App() 
 {
     return (
-        <div>
+        <div className="container">
             <Header />
-            <Pizza />
+            <Menu />
+            <Footer />
         </div>
-    );
-}
-
-function Pizza() 
-{
-    return (
-        <div>
-            <img src='pizzas/spinaci.jpg' alt="pizza spinaci"/>
-            <h1>Pizza Spinaci</h1>
-            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-        </div>
-
     );
 }
 
 function Header() {
+    
     return (
-    <div>
+    <header className="header">
         <h1>Fast React Pizza Co.</h1>
-    </div>
+    </header>
     
 );}
 
 function Menu() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen);
 
+    // if (hour >= openHour && hour <= closeHour)
+    //     alert("We are Open!");
+    // else    
+    //     alert("We are close");
+
+    return(
+        <main className="menu">
+            <h2>Our Menu</h2>
+            <Pizza 
+                    name="Pizza Spinaci"
+                    ingredients = 'Tomato, mozarella, spinach, and ricotta cheese'
+                    photoName='pizzas/spinaci.jpg' 
+                    price={10} />
+
+            <Pizza 
+                    name="Pizza Funghi"
+                    ingredients = 'Tomato, mozarella, mushrooms, and onion'
+                    photoName='pizzas/funghi.jpg' 
+                    price={12} />
+        </main>
+
+);}
+
+function Pizza(props) 
+{
+    console.log(props);
+    return (
+        <div className="pizza">
+            <img src={props.photoName} alt="pizza spinaci"/>
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>{props.price}</span>
+            </div>
+        </div>
+
+    );
 }
 
 function Footer() {
+    return(
+        <div>
+            <footer className="footer">
+            {new Date().toLocaleTimeString()}. We are Currently Open!
+            </footer>
+        </div>
+    );
 
 }
+
+
+
 
 const root =  ReactDOM.createRoot(document.getElementById("root"));
 root.render(

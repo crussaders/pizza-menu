d69cae4938b3d6a8847c502e125fc94da47821ce
Manish Kumar Pandey <896849@cognizant.com>
2024-02-09 20:53:05 +0530

    react fragments

--- src/index.js
+++ src/index.js
@@ -77,6 +77,7 @@ function Menu() {
     return(
         <main className="menu">
             <h2>Our Menu</h2>
+
             {/* conditional rendering with && */}
             {/* {numPizzas > 0 &&  
                 (<ul className="pizzas">
@@ -86,11 +87,19 @@ function Menu() {
 
                     {/* conditional rendenring with ternaries */}
 
-                {numPizzas > 0 ? 
-                    (<ul className="pizzas">
+                {numPizzas > 0 ?  (
+                    //React fragment
+                    <> 
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+                        our stone oven, all organic, all delicious.
+                     </p>
+                    <ul className="pizzas">
                     {pizzas.map((pizza) => 
                     (<Pizza pizzaObj={pizza} key={pizza.name}/>))}
-                    </ul>) : 
+                    </ul>
+                    </>
+                    ) : 
                     (<p>We're still working on our menu. Please comeback later :)</p>)}
             {/* <Pizza 
                     name="Pizza Spinaci"
